553614a266b04f7bb3dfbac232b2359135ab9a4a
goodvandro <goodvandro@gmail.com>
2023-03-06 16:31:32 +0000

    Dynamic tests

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -160,24 +160,20 @@ describe('SignUpComponent', () => {
   });
 
   describe('Validation', () => {
-    it('displays Username is required message when username is null', async () => {
-      await setup();
-      const message = 'Username is required';
-      expect(screen.queryByText(message)).not.toBeInTheDocument();
-      const usernameInput = screen.getByLabelText('Username');
-      await userEvent.click(usernameInput);
-      await userEvent.tab();
-      expect(screen.queryByText(message)).toBeInTheDocument();
-    });
-
-    it('displays length error when username is less then 4 characters', async () => {
-      await setup();
-      const message = 'Username must be at least 4 characters';
-      expect(screen.queryByText(message)).not.toBeInTheDocument();
-      const usernameInput = screen.getByLabelText('Username');
-      await userEvent.type(usernameInput, '123');
-      await userEvent.tab();
-      expect(screen.queryByText(message)).toBeInTheDocument();
-    });
+    it.each`
+      label         | inputValue              | message
+      ${'Username'} | ${'{space}{backspace}'} | ${'Username is required'}
+      ${'Username'} | ${'123'}                | ${'Username must be at least 4 characters'}
+    `(
+      'displays $message when $label has the value "$inputValue"',
+      async ({ label, inputValue, message }) => {
+        await setup();
+        expect(screen.queryByText(message)).not.toBeInTheDocument();
+        const input = screen.getByLabelText(label);
+        await userEvent.type(input, inputValue);
+        await userEvent.tab();
+        expect(screen.queryByText(message)).toBeInTheDocument();
+      }
+    );
   });
 });
