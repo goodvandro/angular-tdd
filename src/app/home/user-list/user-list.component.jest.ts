89d4b428372040c7cd7c592dc01f777a315d723e
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-01-31 11:35:23 +0000

    User list and pagination tests

--- src/app/home/user-list/user-list.component.jest.ts
+++ src/app/home/user-list/user-list.component.jest.ts
@@ -3,7 +3,8 @@ import { setupServer } from 'msw/node';
 import { UserListComponent } from './user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { render, screen, waitFor } from '@testing-library/angular';
-import { getPage } from './user-list.component.spec';
+import userEvent from '@testing-library/user-event';
+import { getPage } from './tset-helper';
 
 const server = setupServer(
   rest.get('/api/1.0/users', (req, res, ctx) => {
@@ -23,14 +24,59 @@ beforeAll(() => server.listen());
 beforeEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-describe('User List', () => {
-  it('displays user list', async () => {
-    await render(UserListComponent, {
-      imports: [HttpClientModule],
-    });
+const setup = async () => {
+  await render(UserListComponent, {
+    imports: [HttpClientModule],
+  });
+};
 
+describe('User List', () => {
+  it('displays three users in list', async () => {
+    await setup();
     await waitFor(() => {
       expect(screen.queryAllByText(/user/).length).toBe(3);
     });
+  });
+  it('displays next page button', async () => {
+    await setup();
+    await screen.findByText('user1');
+    expect(screen.queryByText('next >')).toBeInTheDocument();
+  });
+  it('displays next page after clicking next', async () => {
+    await setup();
+    await screen.findByText('user1');
+    await userEvent.click(screen.getByText('next >'));
+    const firstUserInPage = await screen.findByText('user4');
+    expect(firstUserInPage).toBeInTheDocument();
+  });
+  it('hides next page button at last page', async () => {
+    await setup();
+    await screen.findByText('user1');
+    await userEvent.click(screen.getByText('next >'));
+    await screen.findByText('user4');
+    await userEvent.click(screen.getByText('next >'));
+    await screen.findByText('user7');
+    expect(screen.queryByText('next >')).not.toBeInTheDocument();
+  });
+  it('does not display the previous page button in first page', async () => {
+    await setup();
+    await screen.findByText('user1');
+    expect(screen.queryByText('< previous')).not.toBeInTheDocument();
+  });
+  it('displays previous page button in page 2', async () => {
+    await setup();
+    await screen.findByText('user1');
+    await userEvent.click(screen.getByText('next >'));
+    await screen.findByText('user4');
+    expect(screen.queryByText('< previous')).toBeInTheDocument();
+  });
+  it('displays previous page after clicking previous page button', async () => {
+    await setup();
+    await screen.findByText('user1');
+    await userEvent.click(screen.getByText('next >'));
+    await screen.findByText('user4');
+    await userEvent.click(screen.getByText('< previous'));
+    const firstUserInPage = await screen.findByText('user1');
+    expect(firstUserInPage).toBeInTheDocument();
   });
 });
