687d7a2d2e9c290c909ea645594aff7ceb59830b
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-05-28 11:22:06 +0000

    Disables button when form invalid: jest

--- src/app/login/login.component.jest.ts
+++ src/app/login/login.component.jest.ts
@@ -133,5 +133,29 @@ describe('LoginComponent', () => {
       await screen.findByText('Incorrect Credentials');
       expect(screen.queryByRole('status')).not.toBeInTheDocument();
     });
+    it('does not enable the button when fields are invalid', async () => {
+      await setupForm({ email: 'a' });
+      expect(button).toBeDisabled();
+    });
+  });
+
+  describe('Validation', () => {
+    it.each`
+      label         | inputValue              | message
+      ${'E-mail'}   | ${'{space}{backspace}'} | ${'E-mail is required'}
+      ${'E-mail'}   | ${'wrong-format'}       | ${'Invalid e-mail address'}
+      ${'Password'} | ${'{space}{backspace}'} | ${'Password is required'}
+    `(
+      'displays $message when $label has the value "$inputValue"',
+      async ({ label, inputValue, message }) => {
+        await setup();
+        expect(screen.queryByText(message)).not.toBeInTheDocument();
+        const input = screen.getByLabelText(label);
+        await userEvent.type(input, inputValue);
+        await userEvent.tab();
+        const errorMessage = await screen.findByText(message);
+        expect(errorMessage).toBeInTheDocument();
+      }
+    );
   });
 });
